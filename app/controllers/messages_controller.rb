60481282eb905ac17c89d2fce7012f49b98b409a
masaya <ohmiya_masaya@MacBook-Pro.local>
2020-06-19 01:03:16 +0900

    new, create を実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,14 @@
 class MessagesController < ApplicationController
+    def new
+        @message = Message.new
+    end
+
+    def create
+        Message.create(message_params)
+    end
+
+    private
+    def user_params
+        params.require(:message).permit(:name, :age)
+    end
 end
