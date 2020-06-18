e6dff724a3a01415c9236a308b0a2aeed060550a
Sukitai <kitaitakayoshi@gmail.com>
2020-06-18 12:32:35 +0900

    commit#5 add show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,10 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :age)
