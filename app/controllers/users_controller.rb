4999f58618b6ca161381b72284540d57c2759078
Sukitai <kitaitakayoshi@gmail.com>
2020-06-18 09:40:07 +0900

    commit#3 add new and create views using strong parameter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new(user_params)
+    user.save
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :age)
+  end
 end
