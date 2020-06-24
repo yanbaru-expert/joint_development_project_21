a3f3aa48162975afabac100e4e26a39cf4365a25
Yuko Yamada <oly.oly.onlyno1@gmail.com>
2020-06-24 16:40:45 +0900

    editとupdateの実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,12 +4,25 @@ def index
   @tasks = Task.all
 end
 
+def show
+  @task = Task.find(params[:id])
+end
+
   def new
     @task = Task.new
   end
 
   def create
     Task.create(task_params)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update(task_params)
   end
 
   private
