0889ec6c7dc5b8469f592873ce66c62e620ba1bf
Yuko Yamada <oly.oly.onlyno1@gmail.com>
2020-06-25 12:40:05 +0900

    インデント修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,12 @@
 class TasksController < ApplicationController
 
-def index
-  @tasks = Task.all
-end
+  def index
+    @tasks = Task.all
+  end
 
-def show
-  @task = Task.find(params[:id])
-end
+  def show
+    @task = Task.find(params[:id])
+  end
 
   def new
     @task = Task.new
@@ -24,15 +24,15 @@ end
     task = Task.find(params[:id])
     task.update(task_params)
   end
-
+  
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
   end
 
   private
-   def task_params
-     params.require(:task).permit(:title,:content)
-   end
+  def task_params
+    params.require(:task).permit(:title,:content)
+  end
 
 end
